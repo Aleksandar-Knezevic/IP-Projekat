9704a7e03a0194ac5ec11f04cbdd1267d73e765b
Aleksandar-Knezevic <aleksandar.knezevic97@hotmail.com>
2021-08-27 16:05:45 +0200

    Added backend project

---
+++ IPSpringBackend/src/main/java/net/etfbl/ip/ipspringbackend/IpSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package net.etfbl.ip.ipspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpSpringBackendApplication.class, args);
+    }
+
+}
---
+++ IPSpringBackend/src/test/java/net/etfbl/ip/ipspringbackend/IpSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.etfbl.ip.ipspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
